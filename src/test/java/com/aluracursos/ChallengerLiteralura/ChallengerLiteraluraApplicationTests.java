2481238cecffce160db6cc5dff70236fd0925631
FranGuzGon <fguzmankine@gmail.com>
2024-11-29 16:09:50 -0300

    first commit

---
+++ src/test/java/com/aluracursos/ChallengerLiteralura/ChallengerLiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.ChallengerLiteralura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerLiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
